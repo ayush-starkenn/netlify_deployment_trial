e43d171b6305d967f5ac55777098c3ca9fab92a4
ayush09262001 <ayush@starkenn.com>
2023-09-12 00:32:31 -0700

    message

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/get");
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const save = async () => {
+    try {
+      await axios.post("http://localhost:3001/postData", { title: title });
+      // After saving data, fetch it again to update the UI
+      fetchData();
+    } catch (error) {
+      console.error("Error saving data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // Fetch data when the component mounts
+  }, []);
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="enter note"
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <button onClick={save}>Add</button>
+      {data ? (
+        data.map((ele) => (
+          <div>
+            <p>{ele.title}</p>
+          </div>
+        ))
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
+
+export default App;
